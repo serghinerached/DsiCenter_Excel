0e73f06fa22218de4ac129ab4626190431025b8d
serghinerached <serghinerached81@gmail.com>
2025-06-19 14:32:30 +0200

    commit 10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RequestComponent from "./components/RequestComponent"
-import IncidentComponent from "./components/IncidentComponent"
-import TrackerComponent from "./components/TrackerComponent"
+import RequestComponent from "./components/RequestComponent";
+import IncidentComponent from "./components/IncidentComponent";
+import TrackerComponent from "./components/TrackerComponent";
+import MonthlyIncidentComponent from "./components/monthlyIncidentComponent";
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Route path="/tracker" element={<TrackerComponent />} /> 
         <Route path="/tasks/requests"  element={<RequestComponent />} /> 
         <Route path="/tasks/incidents" element={<IncidentComponent />} /> 
+        <Route path="/reporting/Monthly_Incidents" element={<MonthlyIncidentComponent />} /> 
+
       </Routes>
     </Router>
   );
